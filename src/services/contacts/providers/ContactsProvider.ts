14ffa204a8d76061a455e55285549d8cbd37635a
David Rampling <ramplingd@gmail.com>
2019-11-29 22:04:51 +0000

    Parse the created data with readFile

--- src/services/contacts/providers/ContactsProvider.ts
+++ src/services/contacts/providers/ContactsProvider.ts
@@ -1,9 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+
+import { Contact } from '../../../models/Contact.js'
+
 /**
  * Returns all existing contacts.
  */
 export const getContacts = async (): Promise<any[]> => {
   return new Promise(resolve => {
-    const contacts: any[] = []
-    resolve(contacts)
+    const filepath = path.join(__dirname, '../../../utils/Contacts.json')
+    fs.readFile(filepath, 'utf8', (error, data) => {
+      const result: Contact[] = JSON.parse(data)
+      resolve(result)
+    })
   })
 }
